47e4f7529c0310f83e563e35651028351278f7c9
Ewelina <ewelina@next.biz.pl>
2018-12-10 16:07:11 +0100

    2

---
+++ ClientTCPApp/ClientTCPApp/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientTCPApp
+{
+    enum Command
+    {
+        //Log into the server
+        Login,
+        //Logout of the server
+        Logout,
+        //Send a text message to all the chat clients     
+        Message,
+        //Get a list of users in the chat room from the server
+        List
+    }
+
+  
+    class Client
+    {
+    }
+}
